ce31f939060538b961953cc469c149ae0c3ce1d3
QuangNguyeen <nguyenvanquangngunguoi@gmail.com>
2024-05-30 02:25:03 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import TodoFeature from "./features/Todo/pages";
 
 function App() {
-  const name = "Quang"
-  const age = 19
-  const isMale = true
-  const student = {
-    name: 'Easy FrontEnd'
-  }
-  const colorList = ['green', 'blue', 'red'];
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-            <p>Xin chao {name} - {age} - {isMale ? "Male":"Female"}</p>
-        </p>
-        <ul>
-          {colorList.map((color) => (
-              <li key = {color} style={{color}}>{color}</li>
-          ))}
-        </ul>
-      </header>
+      <TodoFeature></TodoFeature>
     </div>
   );
 }
